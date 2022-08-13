8dfa458ca51ff7d15f3f8be34110a9b3865ede63
Nikola Ajzenhamer (ajzenhamernikola) <ajzenhamernikola@gmail.com>
2022-08-13 15:46:02 +0200

    Added an enum for domain names

---
+++ configs/domain_list.py
@@ -0,0 +1,14 @@
+from core.strenum import StrEnum
+
+
+class DomainList(StrEnum):
+    PEOPLE = "people",
+    COMMENTS = "comments",
+    PROJECTS = "projects",
+    EVENTS = "events",
+    TEAMS = "teams",
+    TASKS = "tasks",
+    CONTACTS = "contacts",
+    COMPANIES = "companies",
+    ROOMS = "rooms",
+    MESSAGES = "messages"
